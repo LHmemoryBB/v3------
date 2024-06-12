06ebb189a308a96d0d688d67e0bcef6534f858ef
lokLi <1925792024@qq.com>
2024-06-13 00:00:55 +0800

    ·

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>欢迎来到首页</div>
+</template>
---
+++ src/views/login/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="login-container">
+    <div class="login-card">
+      <div class="title">
+        <img src="@/assets/layouts/logo-text-2.png" />
+      </div>
+      <div class="content">
+        <el-form ref="loginFormRef" :model="loginFormData" :rules="loginFormRules" @keyup.enter="handleLogin">
+          <el-form-item prop="username">
+            <el-input
+              v-model.trim="loginFormData.username"
+              placeholder="用户名"
+              type="text"
+              tabindex="1"
+              :prefix-icon="User"
+              size="large"
+            />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              v-model.trim="loginFormData.password"
+              placeholder="密码"
+              type="password"
+              tabindex="2"
+              :prefix-icon="Lock"
+              size="large"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item prop="code">
+            <Verify
+              @success="success"
+              mode="pop"
+              captchaType="blockPuzzle"
+              :imgSize="{ width: '330px', height: '155px' }"
+              ref="verify"
+            />
+          </el-form-item>
+          <el-button :loading="loading" type="primary" size="large" @click.prevent="handleLogin">登 录</el-button>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue"
+import { useRouter } from "vue-router"
+import { useUserStore } from "@/store/modules/user"
+import { type FormInstance, type FormRules } from "element-plus"
+import { User, Lock, Key, Picture, Loading } from "@element-plus/icons-vue"
+import { getLoginCodeApi } from "@/api/login"
+import { type LoginRequestData } from "@/api/login/types/login"
+import Verify from "@/components/verifition/Verify.vue"
+const router = useRouter()
+import md5 from "js-md5"
+/** 登录表单元素的引用 */
+const loginFormRef = ref<FormInstance | null>(null)
+
+/** 登录按钮 Loading */
+const loading = ref(false)
+/** 验证码图片 URL */
+const verify = ref(null)
+/** 登录表单数据 */
+const loginFormData: LoginRequestData = reactive({
+  username: "lh",
+  password: "123456",
+  captchaVerification: ""
+})
+/** 登录表单校验规则 */
+const loginFormRules: FormRules = {
+  username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+  password: [
+    { required: true, message: "请输入密码", trigger: "blur" },
+    { min: 3, max: 16, message: "长度在 3 到 16 个字符", trigger: "blur" }
+  ]
+}
+/** 登录逻辑 */
+const handleLogin = () => {
+  loginFormRef.value?.validate((valid: boolean, fields) => {
+    if (valid) {
+      verify?.value.show()
+    } else {
+      console.error("表单校验不通过", fields)
+    }
+  })
+}
+const success = (res) => {
+  console.log(res, "新")
+  loginFormData.captchaVerification = res.captchaVerification
+  loginFormData.password = md5(loginFormData.password)
+  useUserStore()
+    .login(loginFormData)
+    .then(() => {
+      router.push({ path: "/" })
+    })
+    .catch(() => {
+      loginFormData.password = ""
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100%;
+  .theme-switch {
+    position: fixed;
+    top: 5%;
+    right: 5%;
+    cursor: pointer;
+  }
+  .login-card {
+    width: 480px;
+    max-width: 90%;
+    border-radius: 20px;
+    box-shadow: 0 0 10px #dcdfe6;
+    background-color: var(--el-bg-color);
+    overflow: hidden;
+    .title {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 150px;
+      img {
+        height: 100%;
+      }
+    }
+    .content {
+      padding: 20px 50px 50px 50px;
+      :deep(.el-input-group__append) {
+        padding: 0;
+        overflow: hidden;
+        .el-image {
+          width: 100px;
+          height: 40px;
+          border-left: 0px;
+          user-select: none;
+          cursor: pointer;
+          text-align: center;
+        }
+      }
+      .el-button {
+        width: 100%;
+        margin-top: 10px;
+      }
+    }
+  }
+}
+</style>
